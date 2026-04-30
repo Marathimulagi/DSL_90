a86da78a29fffc274975e4a865e1b82225a01f64
Marathimulagi <tejaswinidchavan2311@gmail.com>
2026-04-30 11:13:46 +0530

    Create internal exam.cpp

---
+++ internal exam.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+Node* createNode() {
+    int value;
+    cout << "Enter value:";
+    cin >> value;
+
+    if (value == -1)
+        return NULL;
+
+    Node* root = createNode(value);
+
+    cout << "Enter left child of " << value << endl;
+    root->left =createNode();
+
+    cout << "Enter right child of " << value << endl;
+    root->right =createNode();
+
+    return root;
+}
+
+void preorder(Node* root) {
+    if (root == NULL) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    cout << "createNode:\n";
+    Node* root =createNode();
+
+    cout << "\nPreorder: ";
+    preorder(root);
+
+    cout << "\nInorder: ";
+    inorder(root);
+
+    cout << "\nPostorder: ";
+    postorder(root);
+
+    return 0;
+}
